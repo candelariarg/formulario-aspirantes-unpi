7f859093de5e7c9c7233e8e754773f7be4ec3ba6
Mashuu <maurex05@gmail.com>
2026-08-20 22:47:20 -0300

    actualizacion datosPersonales

--- src/componentes/formulario/Base.jsx
+++ src/componentes/formulario/Base.jsx
@@ -31,7 +31,7 @@ export function BaseForm() {
                 <EnviarCV />
             </div>
 
-        
+            
 
 
 
--- src/componentes/formulario/DatosPersonales.jsx
+++ src/componentes/formulario/DatosPersonales.jsx
@@ -1,7 +1,27 @@
-export function DatosPersonales(){
-    return(
+export function DatosPersonales() {
+    return (
         <>
-        <div className="datos-personales">
+            <div className="datos-personales">
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="">Nombre</span>
+                    </div>
+                    <input type="text" className="form-control" placeholder="Ingrese su nombre"/>
+
+                    <div className="input-group">
+                        <span className="input-group-text" id="">Apellido</span>
+                    </div>
+                    <input type="text" className="form-control" placeholder="Ingrese su apellido"/>
+                    <div className="input-group">
+                        <span className="input-group-text" id="">DNI</span>
+                    </div>
+                    <input type="text" className="form-control" placeholder="Ingrese su DNI"/>
+                    <div className="input-group">
+                        <span className="input-group-text" id="">telefono</span>
+                    </div>
+                    <input type="text" className="form-control" placeholder="Ingrese su telefono"/>
+                </div>
+                
                 <form >
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Correo electronico</label>
