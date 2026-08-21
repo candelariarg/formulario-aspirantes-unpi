21feb5196a24aa7ae8f94a2da2286f61f4d8e568
Mashuu <maurex05@gmail.com>
2026-08-21 14:41:40 -0300

    actualizacion formacion_maxima

--- src/componentes/formulario/formacion_maxima.jsx
+++ src/componentes/formulario/formacion_maxima.jsx
@@ -3,17 +3,34 @@ export function FormacionAcademica() {
         <>
             <form>
                 <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                    <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="Pregrado" />
+                    <label className="form-check-label" htmlFor="inlineCheckbox1">Pregrado</label>
                 </div>
                 <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                    <label className="form-check-label" htmlFor="inlineCheckbox2">2</label>
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="Grado" />
+                    <label className="form-check-label" htmlFor="inlineCheckbox2">Grado</label>
                 </div>
                 <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" disabled />
-                    <label className="form-check-label" htmlFor="inlineCheckbox3">3 (disabled)</label>
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="Especializacion"  />
+                    <label className="form-check-label" htmlFor="inlineCheckbox3">Especializacion</label>
                 </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="Maestria"  />
+                    <label className="form-check-label" htmlFor="inlineCheckbox3">Maestria</label>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox4" value="Doctorado"  />
+                        <label className="form-check-label" htmlFor="inlineCheckbox4">Doctorado</label>
+                    </div>
+                </div>
+            </form>
+            <form>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id=""><i class="bi bi-bookmark"></i></span>
+                    </div>
+                    <input type="text" className="form-control" placeholder="Otras formaciones"/>
+                </div>
+                <input className="linkedin-input" type="text" placeholder="url de perfil Linkedin" />
             </form>
         </>
     )
