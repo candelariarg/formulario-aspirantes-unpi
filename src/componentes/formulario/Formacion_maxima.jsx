e7863531397fdb7765c214ff5606f8765c02d9d6
Mashuu <maurex05@gmail.com>
2026-09-08 14:11:53 -0300

    Actualizacion formacion academica

--- src/componentes/formulario/Formacion_maxima.jsx
+++ src/componentes/formulario/Formacion_maxima.jsx
@@ -1,39 +1,60 @@
+import React from "react";
+
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import { colourOptions } from "../../docs/data.js";
+
+const animatedComponents = makeAnimated();
 export function FormacionAcademica({ user, onChange }) {
-    // aca utilizo inputs de tipo radio y texto para poder asignar los valores de estudio maximo que tiene el usuario y experiencia en docencia
-    // se utiliza el checked para que el input se marque si el valor es igual al valor del input tambien utilizamos el tipo
-    // radio porque es para que el usuario pueda elegir solo una opcion de las que se le presentan 
+  // aca utilizo inputs de tipo radio y texto para poder asignar los valores de estudio maximo que tiene el usuario y experiencia en docencia
+  // se utiliza el checked para que el input se marque si el valor es igual al valor del input tambien utilizamos el tipo
+  // radio porque es para que el usuario pueda elegir solo una opcion de las que se le presentan
 
-    return (
-        <>
-            <h5>Formación Académica</h5>
-            <div>
-                <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
-                    <label className="form-label d-block">Nivel máximo de titulación *</label>
-                    <select
-                        className="form-select"
-                        name="formacion"
-                        value={user?.formacion || ""}
-                        onChange={onChange}
-                    >
-                        <option value="">Seleccionar...</option>
-                        <option value="Pregrado">Pregrado</option>
-                        <option value="Grado">Grado</option>
-                        <option value="Especializacion">Especialización</option>
-                        <option value="Maestria">Maestría</option>
-                        <option value="Doctorado">Doctorado</option>
-                    </select>
-                </div>
+  return (
+    <>
+      <h5>Formación Académica</h5>
+      <div>
+        <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
+          <label className="form-label d-block">
+            Nivel máximo de titulación *
+          </label>
+          <select
+            className="form-select"
+            name="formacion"
+            value={user?.formacion || ""}
+            onChange={onChange}
+          >
+            <option value="">Seleccionar...</option>
+            <option value="Pregrado">Pregrado</option>
+            <option value="Grado">Grado</option>
+            <option value="Especializacion">Especialización</option>
+            <option value="Maestria">Maestría</option>
+            <option value="Doctorado">Doctorado</option>
+          </select>
+        </div>
+        <Select
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          defaultValue={[colourOptions[4], colourOptions[5]]}
+          isMulti
+          options={colourOptions}
+        />
+        <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
+          {/* Columna para Otras Formaciones */}
 
-                <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
-                    {/* Columna para Otras Formaciones */}
-                    
-                        <label className="form-label" id="">Otras Formaciones <i className="bi bi-bookmark"></i></label>
-                        
-                        <input type="text" className="form-control" name="otrasFormaciones" value={user?.otrasFormaciones || ""} onChange={onChange} />
-                </div>
+          <label className="form-label" id="">
+            Otras Formaciones <i className="bi bi-bookmark"></i>
+          </label>
 
-
-            </div>
-        </>
-    )
+          <input
+            type="text"
+            className="form-control"
+            name="otrasFormaciones"
+            value={user?.otrasFormaciones || ""}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
