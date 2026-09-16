0ba38768b879ecc81cbbdaf2022ae0f414a5d509
Mashuu <maurex05@gmail.com>
2026-09-16 17:40:04 -0300

    actualizacion de especialidades

--- src/componentes/formulario/Formacion_maxima.jsx
+++ src/componentes/formulario/Formacion_maxima.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
-import { colourOptions } from "../../docs/data.js";
+import { especialidades } from "../../docs/data.js";
 
 const animatedComponents = makeAnimated();
 export function FormacionAcademica({ user, onChange }) {
@@ -13,48 +13,53 @@ export function FormacionAcademica({ user, onChange }) {
   return (
     <>
       <h5>Formación Académica</h5>
-      <Select
-        closeMenuOnSelect={false}
-        components={animatedComponents}
-        defaultValue={[colourOptions[4], colourOptions[5]]}
-        isMulti
-        options={colourOptions}
-      />
-      <div>
-        <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
-          <label className="form-label d-block">
-            Nivel máximo de titulación *
-          </label>
-          <select
-            className="form-select"
-            name="formacion"
-            value={user?.formacion || ""}
-            onChange={onChange}
-          >
-            <option value="">Seleccionar...</option>
-            <option value="Pregrado">Pregrado</option>
-            <option value="Grado">Grado</option>
-            <option value="Especializacion">Especialización</option>
-            <option value="Maestria">Maestría</option>
-            <option value="Doctorado">Doctorado</option>
-          </select>
-        </div>
-
-        <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
-          {/* Columna para Otras Formaciones */}
-
-          <label className="form-label" id="">
-            Otras Formaciones <i className="bi bi-bookmark"></i>
-          </label>
-
-          <input
-            type="text"
-            className="form-control"
-            name="otrasFormaciones"
-            value={user?.otrasFormaciones || ""}
-            onChange={onChange}
+      <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
+        <label className="form-label d-block">Especialidades *</label>
+        <div style={{ maxWidth: "800px", padding: "1rem" }}>
+          <Select
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            defaultValue={[especialidades[4], especialidades[5]]}
+            isMulti
+            options={especialidades}
+            placeholder={'Seleccione...'}
           />
         </div>
+      </div>
+
+      <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
+        <label className="form-label d-block">
+          Nivel máximo de titulación *
+        </label>
+        <select
+          className="form-select"
+          name="formacion"
+          value={user?.formacion || ""}
+          onChange={onChange}
+        >
+          <option value="">Seleccionar...</option>
+          <option value="Pregrado">Pregrado</option>
+          <option value="Grado">Grado</option>
+          <option value="Especializacion">Especialización</option>
+          <option value="Maestria">Maestría</option>
+          <option value="Doctorado">Doctorado</option>
+        </select>
+      </div>
+
+      <div className="mb-3 mx-auto" style={{ maxWidth: "800px" }}>
+        {/* Columna para Otras Formaciones */}
+
+        <label className="form-label" id="">
+          Otras Formaciones <i className="bi bi-bookmark"></i>
+        </label>
+
+        <input
+          type="text"
+          className="form-control"
+          name="otrasFormaciones"
+          value={user?.otrasFormaciones || ""}
+          onChange={onChange}
+        />
       </div>
     </>
   );
